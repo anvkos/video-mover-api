58d78b5072a59cd3552a962f5168b101e46ed9e8
Anton Kosolapov <anvkos@ya.ru>
2018-09-02 20:48:10 +0700

    Save task wrapped in transaction

--- app/Services/CreateTaskService.php
+++ app/Services/CreateTaskService.php
@@ -5,24 +5,29 @@ namespace App\Services;
 use App\Models\Task;
 use App\Models\Storage;
 use App\Models\StorageTask;
+use Illuminate\Support\Facades\DB;
 
 class CreateTaskService
 {
-    public function perform(Array $params)
+    public function perform(Array $params): Task
     {
-        $task = Task::create([
-            'title' => $params['title'],
-            'enabled' => $params['enabled'],
-            'status' => Task::STATUS_PENDING,
-            'action' => $params['action'],
-            'number_files' => $params['number_files'],
-        ]);
-        foreach ($params['src_storage_ids'] as $id) {
-            $task->storages()->attach($id, ['kind' => StorageTask::KIND_SOURCE]);
-        }
-        foreach ($params['dst_storage_ids'] as $id) {
-            $task->storages()->attach($id, ['kind' => StorageTask::KIND_DESTINATION]);
-        }
-        return $task;
+        return DB::transaction(function () use ($params) {
+            $task = Task::create([
+                'title' => $params['title'],
+                'enabled' => $params['enabled'],
+                'status' => Task::STATUS_PENDING,
+                'action' => $params['action'],
+                'number_files' => $params['number_files'],
+                'size_files' => $params['size_files'],
+            ]);
+            foreach ($params['src_storage_ids'] as $id) {
+                $task->storages()->attach($id, ['kind' => StorageTask::KIND_SOURCE]);
+            }
+            foreach ($params['dst_storage_ids'] as $id) {
+                $task->storages()->attach($id, ['kind' => StorageTask::KIND_DESTINATION]);
+            }
+            $task->criteria()->createMany($params['criteria']);
+            return $task;
+        });
     }
 }
--- app/Services/UpdateTaskService.php
+++ app/Services/UpdateTaskService.php
@@ -5,21 +5,27 @@ namespace App\Services;
 use App\Models\Task;
 use App\Models\Storage;
 use App\Models\StorageTask;
+use Illuminate\Support\Facades\DB;
 
 class UpdateTaskService
 {
-    public function perform(int $taskId, Array $params)
+    public function perform(int $taskId, Array $params): Task
     {
-        $task = Task::findOrFail($taskId);
-        $task->update([
-            'title' => $params['title'],
-            'enabled' => $params['enabled'],
-            'action' => $params['action'],
-            'number_files' => $params['number_files'],
-        ]);
-        $srcStorages = array_fill_keys($params['src_storage_ids'], ['kind' => StorageTask::KIND_SOURCE]);
-        $dstStorages = array_fill_keys($params['dst_storage_ids'], ['kind' => StorageTask::KIND_DESTINATION]);
-        $task->storages()->sync($srcStorages + $dstStorages);
-        return $task;
+        return DB::transaction(function () use ($taskId, $params) {
+            $task = Task::findOrFail($taskId);
+            $task->update([
+                'title' => $params['title'],
+                'enabled' => $params['enabled'],
+                'action' => $params['action'],
+                'number_files' => $params['number_files'],
+                'size_files' => $params['size_files'],
+            ]);
+            $srcStorages = array_fill_keys($params['src_storage_ids'], ['kind' => StorageTask::KIND_SOURCE]);
+            $dstStorages = array_fill_keys($params['dst_storage_ids'], ['kind' => StorageTask::KIND_DESTINATION]);
+            $task->storages()->sync($srcStorages + $dstStorages);
+            $task->criteria()->delete();
+            $task->criteria()->createMany($params['criteria']);
+            return $task;
+        });
     }
 }
